e77b0df0789d9c2bb40906333eedae99573b3f3a
IsaacStudley <studleyisaac@gmail.com>
2024-12-04 07:57:56 -0500

    c

---
+++ programs/CLISUM.C
@@ -0,0 +1,26 @@
+#include <iostream> //12/4 and what includes to use
+using namespace std;
+
+main() //12/4 looking in how to handle negatives and non ints
+{
+   int a, b;
+
+    // Input two integers
+    cout << "Enter two integers: "; 
+    cin >> a >> b;
+  
+      // If b is positive, increment a b times
+    for (int i = 0; i < b; i++) {
+        a++;
+    }
+    
+    // If b is negative, decrement a |b| times
+    for (int i = 0; i > b; i--) {
+        a--;
+    }
+
+    // Output the sum
+    cout << "Sum = " << a << endl;
+
+    return 0;
+}
