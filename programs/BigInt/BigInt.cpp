3ea234ab1b3ae7c8dfc28ede4b8ca4ce4e0bbe2e
IsaacStudley <studleyisaac@gmail.com>
2025-01-24 09:03:44 -0500

    bigintmultiply

--- programs/BigInt/BigInt.cpp
+++ programs/BigInt/BigInt.cpp
@@ -38,19 +38,6 @@ string sum_common_len_digit_strs(const string &s1, const string &s2)
     return carry ? "c+" + sum : sum;
 }
 
-string dif_common_len_digit_strs(const string &s1, const string &s2)
-{
-    char digit_dif, carry = 0;
-    string dif = s1;
-
-    for (int i = s1.size() - 1; i >= 0; i--) {
-        digit_dif = to_num(s1[i]) + to_num(s2[i]) + carry; 
-        dif[i] = digit_to_char(digit_dif % 10);
-        carry = digit_dif > 9 ? 1 : 0;
-    }
-
-    return carry ? "c+" + dif : dif;
-}
 
 BigInt::BigInt()
 {
@@ -75,10 +62,89 @@ string BigInt::to_string() const
     return (!negative) ? digits : "-" + digits;
 }
 
+bool BigInt::operator>(const BigInt& i2) const
+{
+    if (!is_neg && i2.is_neg) return true;
+    if (is_neg && !i2.is_neg) return false;
+    // They have the same sign
+    if (digits.size() > i2.digits.size()) {
+        return is_neg ? false : true;
+    }
+    if (digits.size() < i2.digits.size()) {
+        return is_neg ? true : false;
+    }
+    // They have the same number of digits 
+    for (int i = 0; i < digits.size(); i++) {
+        if (digits[i] > i2.digits[i]) {
+            return is_neg ? false : true;
+        }
+    }
+    // They are equal
+    return false;
+}
+
+bool BigInt::operator!=(const BigInt& i2) const
+{
+    return !(this->operator==(i2));
+}
+
+bool BigInt::operator<=(const BigInt& i2) const
+{
+    return !(this->operator>(i2));
+}
+
+bool BigInt::operator<(const BigInt& i2) const
+{
+    return !(this->operator==(i2)) && !(this->operator>(i2));
+}
+
+bool BigInt::operator>=(const BigInt& i2) const
+{
+    return (this->operator>(i2)) || (this->operator==(i2));
+}
+
+BigInt BigInt::operator+(const BigInt& i2) const
+{
+    if ((*this).digits.size() == i2.digits.size()) {
+        string raw_sum = sum_common_len_digit_strs((*this).digits, i2.digits);
+        if (raw_sum[0] == 'c')
+            return BigInt("1" + raw_sum.substr(2));
+        return BigInt(raw_sum);
+    }
+        
+    // Addends have different numbers of digits
+    const BigInt *longer;
+    const BigInt *shorter;
+    int common, extra;
+    string summed_common_digits, leading_digits;
+
+    if ((*this).digits.size() > i2.digits.size()) {
+        longer = this;
+        shorter = &i2;
+    } else {
+        longer = &i2;
+        shorter = this;
+    };
+
+    common = shorter->digits.size();
+    extra = longer->digits.size() - common;
+    summed_common_digits = sum_common_len_digit_strs(
+       shorter->digits, 
+       longer->digits.substr(extra)
+    );
+    leading_digits = longer->digits.substr(0, extra);
+
+    if (summed_common_digits[0] != 'c')
+        return BigInt(leading_digits + summed_common_digits);
+
+    return BigInt(increment_digit_string(leading_digits) +
+                  summed_common_digits.substr(2));
+}
+
 BigInt BigInt::operator-(const BigInt& i2) const
 {
     //i2 = BigInt(-i2);
-    BigInt i2(-i2); // trying to make i2 negative and keep the rest the same
+    BigInt i2("-"+i2.digits); // trying to make i2 negative and keep the rest the same
     if ((*this).digits.size() == i2.digits.size()) {
         string raw_sum = sum_common_len_digit_strs((*this).digits, i2.digits);
         if (raw_sum[0] == 'c')
@@ -117,41 +183,10 @@ BigInt BigInt::operator-(const BigInt& i2) const
 
 BigInt BigInt::operator*(const BigInt& i2) const
 {
-    for (int i = ; i >= 0; i--) { // 1/23 something that is just the numerical i2
-  
-    if ((*this).digits.size() == i2.digits.size()) {
-        string raw_sum = sum_common_len_digit_strs((*this).digits, i2.digits);
-        if (raw_sum[0] == 'c')
-            return BigInt("1" + raw_sum.substr(2));
-        return BigInt(raw_sum);
+    BigInt i3((*this).digits);
+    BigInt final_value(0);
+    for (BigInt i = i2.digits; i >= 0; i-1) { // 1/23 something that is just the numerical i2
+    final_value = final_value+i3;
     }
-        
-    // Addends have different numbers of digits
-    const BigInt *longer;
-    const BigInt *shorter;
-    int common, extra;
-    string summed_common_digits, leading_digits;
-
-    if ((*this).digits.size() > i2.digits.size()) {
-        longer = this;
-        shorter = &i2;
-    } else {
-        longer = &i2;
-        shorter = this;
-    };
-
-    common = shorter->digits.size();
-    extra = longer->digits.size() - common;
-    summed_common_digits = sum_common_len_digit_strs(
-       shorter->digits, 
-       longer->digits.substr(extra)
-    );
-    leading_digits = longer->digits.substr(0, extra);
-
-    if (summed_common_digits[0] != 'c')
-        return BigInt(leading_digits + summed_common_digits);
-
-    return BigInt(increment_digit_string(leading_digits) +
-                  summed_common_digits.substr(2));
-}
+    return BigInt(final_value); // 1/24 this may only work right with positive numbers
 }
