a6067a4d19ba0dd052e8b3445a2328aa7f11ce25
IsaacStudley <studleyisaac@gmail.com>
2025-01-24 15:35:49 -0500

    bigintdivision

--- programs/BigInt/BigInt.cpp
+++ programs/BigInt/BigInt.cpp
@@ -175,3 +175,19 @@ BigInt BigInt::operator*(const BigInt& i2) const
     }
     return BigInt(final_value); 
 }
+
+BigInt BigInt::operator/(const BigInt& i2) const // 1/24 this is division with truncation
+{
+    BigInt i3((*this).digits);
+    BigInt final_value(0);
+    BigInt counter(0);
+    BigInt i = i2.digits;
+    while (counter<=i3) { // 1/24 adds i over and over and sees how many times it takes to equal *this
+    final_value = final_value+1;
+    counter = counter+i;
+    }
+    if (i2.negative != (*this).negative){
+        final_value.negative = true; // 1/24 accounting for negative numbers
+    }
+    return BigInt(final_value); 
+}
